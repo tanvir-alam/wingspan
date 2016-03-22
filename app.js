cbdfbebc2eccd44074b5951403fb3971bb035b70
tamit <tamit@loyalty.com>
2016-03-22 17:42:57 -0400

    added basic form UI

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-(function () {
-  'use strict';
-  var express = require('express');
-  var app = express();
+// module.exports = function(app) {
 
-  require('./config/config_app')(app);
-  require('./config/config_routes')(app);
+//   app.get('/api/v1/cities', function(req,res) {
+//     sabreCall('/v1/lists/supported/cities', res);
+//   });
 
-  // START THE SERVER
-  console.log('STARTING THE SABRE SERVER');
-  console.log('-------------------------');
-  app.listen(3000);
-  console.log('Started the server');
-  process.on('uncaughtException', function (error) {
-      console.log(error.stack);
-      console.log(error);
-  });
+//   app.get('/api/v1/places', function(req,res) {
+//     sabreCall('/v1/shop/flights/fares?origin=' + req.query.origin +
+//     '&departuredate=' + req.query.departuredate +
+//     '&returndate=' + req.query.returndate +
+//     '&maxfare=' + req.query.maxfare, res);
+//   });
+// };
 
-})();
