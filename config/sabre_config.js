8edbf12152c4a12e8d4f28619b4e21c3a6564c65
tamit <tamit@loyalty.com>
2016-03-24 16:52:25 -0400

    separated sabre_config from routes

--- config/sabre_config.js
+++ config/sabre_config.js
@@ -1,43 +1,5 @@
-var sabreDevStudio = require('sabre-dev-studio');
-var sabre = new sabreDevStudio({
+module.exports = {
   client_id:     'V1:en9q4agampnnihdz:DEVCENTER:EXT',
   client_secret: 'H9tjv9YR',
   uri:           'https://api.test.sabre.com'
-});
-var options = {};
-
-function sabreCall(q, res) {
-  sabre.get(q, options, function(err, data) {
-    response(res, err, data);
-  });
 }
-
-function response(res, err, data) {
-  if (err) {
-    res.status(200).send({
-      'status': false,
-      'message': 'Error',
-      'info': err
-    });
-  } else {
-    res.status(200).send({
-      'status': true,
-      'message': 'Success',
-      'info': data
-    });
-  }
-}
-
-module.exports = function(app) {
-
-  app.get('/api/v1/cities', function(req,res) {
-    sabreCall('/v1/lists/supported/cities', res);
-  });
-
-  app.get('/api/v1/places', function(req,res) {
-    sabreCall('/v1/shop/flights/fares?origin=' + req.query.origin +
-    '&departuredate=' + req.query.departuredate +
-    '&returndate=' + req.query.returndate +
-    '&maxfare=' + req.query.maxfare, res);
-  });
-};
---
+++ routes.js
@@ -0,0 +1,40 @@
+var sabreDevStudio = require('sabre-dev-studio');
+var sabreConfig = require('./config/sabre_config');
+var sabre = new sabreDevStudio(sabreConfig);
+var options = {};
+
+function sabreCall(q, res) {
+  sabre.get(q, options, function(err, data) {
+    response(res, err, data);
+  });
+}
+
+function response(res, err, data) {
+  if (err) {
+    res.status(200).send({
+      'status': false,
+      'message': 'Error',
+      'info': err
+    });
+  } else {
+    res.status(200).send({
+      'status': true,
+      'message': 'Success',
+      'info': data
+    });
+  }
+}
+
+module.exports = function(app) {
+
+  app.get('/api/v1/cities', function(req,res) {
+    sabreCall('/v1/lists/supported/cities', res);
+  });
+
+  app.get('/api/v1/places', function(req,res) {
+    sabreCall('/v1/shop/flights/fares?origin=' + req.query.origin +
+    '&departuredate=' + req.query.departuredate +
+    '&returndate=' + req.query.returndate +
+    '&maxfare=' + req.query.maxfare, res);
+  });
+};
